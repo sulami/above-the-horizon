b46131d772cae9fad3bed68a1f59a2fc25ae82c6
Robin Schroer <sulami@peerwire.org>
2018-03-30 19:33:51 +0100

    Add some mock code that does some realm storage and retrieval

--- src/above_the_horizon/db.cljs
+++ src/above_the_horizon/db.cljs
@@ -4,6 +4,37 @@
 
 (def realm (js/require "realm"))
 
+(defn jsx->clj
+  [x]
+  (if (map? x)
+    x
+    (into {} (for [k (.keys js/Object x)]
+               [(keyword k)
+                (let [v (aget x k)]
+                  (if (instance? realm.List v)
+                    (map jsx->clj (array-seq v))
+                    v))]))))
+
+(defn query
+  [r table]
+  (some->> (.objects r table)
+           array-seq
+           (mapv jsx->clj)))
+
+(defn do-stuff []
+  (let* [schema [{:name "Task", :properties {:name "string"}}]
+         r (new realm (clj->js {:schema schema}))
+         old-tasks (.objects r "Task")
+         new-task (clj->js {:name "Fly away"})]
+    (.write r (fn []
+                (.delete r old-tasks)
+                (.create r "Task" (clj->js {:name "something esle"}))
+                (.create r "Task" new-task)))
+    (let [got-task (query r "Task")]
+      (prn got-task)
+      (.close r)
+      got-task)))
+
 ;; spec of app-db
 ;; (sp/def ::tasks )
 (sp/def ::app-db
